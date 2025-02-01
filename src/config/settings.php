7e05cf52ca57ad4fb80a388892a3e125d4f23766
Abdul-Aziz-dal <95477937+Abdul-Aziz-dal@users.noreply.github.com>
2025-02-01 16:21:02 +0500

    cors error resolved

--- src/config/settings.php
+++ src/config/settings.php
@@ -1,3 +1,7 @@
 <?php
 ini_set('display_errors', 0);
 error_reporting(E_ALL);
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
