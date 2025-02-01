3872e71e193a6562e1789f6685cacfac020e446a
Abdul-Aziz-dal <95477937+Abdul-Aziz-dal@users.noreply.github.com>
2025-02-01 16:30:33 +0500

    cors error removed

--- src/api/auth/login.php
+++ src/api/auth/login.php
@@ -6,6 +6,8 @@ require_once '../../config/settings.php';
 //****Headers*****//
 header('Content-Type:application/json');
 header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
 //*****CheckPostRequest*******//
 if ($_SERVER['REQUEST_METHOD'] != "POST") {
--- src/config/settings.php
+++ src/config/settings.php
@@ -1,7 +1,3 @@
 <?php
 ini_set('display_errors', 0);
 error_reporting(E_ALL);
-
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
