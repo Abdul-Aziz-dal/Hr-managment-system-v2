e44e14b5d91247c4b76818f0d527e79b2a922fe6
Abdul-Aziz-dal <95477937+Abdul-Aziz-dal@users.noreply.github.com>
2025-02-01 17:00:27 +0500

    Update login.php

--- src/api/auth/login.php
+++ src/api/auth/login.php
@@ -6,14 +6,6 @@ require_once '../../config/settings.php';
 //****Headers*****//
 header('Content-Type:application/json');
 header('Access-Control-Allow-Origin: *');
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
-
-//*****Handle OPTIONS (CORS Preflight Request)******//
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    http_response_code(200);  // Allow preflight request
-    exit();
-}
 
 //*****CheckPostRequest*******//
 if ($_SERVER['REQUEST_METHOD'] != "POST") {
@@ -29,9 +21,10 @@ if ($_SERVER['REQUEST_METHOD'] != "POST") {
 require_once '../../classes/Auth.class.php';
 
 //****Get Raw JSON Request Body*****//
-$data = json_decode(file_get_contents("php://input"), true);
+$data =  $_POST;
 
 //****CheckParams*****//
+
 if (!isset($data['email']) || !isset($data['password'])) {
     echo json_encode(array(
         "status" => false,
@@ -41,9 +34,11 @@ if (!isset($data['email']) || !isset($data['password'])) {
     die;
 }
 
+
 //*****GettingParams*******//
 $email = $data['email'];
 $password = $data['password'];
 
 $auth = new Auth();
+
 $auth->verifyLogin($email, $password);
