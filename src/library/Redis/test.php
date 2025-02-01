438cc0cc33b07d01ce50193b347d00335c34b234
Abdul-Aziz-dal <95477937+Abdul-Aziz-dal@users.noreply.github.com>
2025-02-01 20:12:05 +0500

    Update test.php

--- src/library/Redis/test.php
+++ src/library/Redis/test.php
@@ -2,7 +2,7 @@
 <?php
 $redis = new Redis();
 try {
-    $redis->connect('127.0.0.1', 6379);
+    $redis->connect('127.0.0.1:7379', 6379);
     echo "Redis Connection: " . $redis->ping();
 } catch (Exception $e) {
     echo "Error: " . $e->getMessage();
