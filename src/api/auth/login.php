b9c9b7573b13b7ad4ff70ea4d09a56b8082b53f5
Abdul-Aziz-dal <95477937+Abdul-Aziz-dal@users.noreply.github.com>
2025-02-01 16:38:04 +0500

    Update login.php

--- src/api/auth/login.php
+++ src/api/auth/login.php
@@ -9,6 +9,12 @@ header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
+//*****Handle OPTIONS (CORS Preflight Request)******//
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    http_response_code(200);  // Allow preflight request
+    exit();
+}
+
 //*****CheckPostRequest*******//
 if ($_SERVER['REQUEST_METHOD'] != "POST") {
     echo json_encode(array(
@@ -23,24 +29,21 @@ if ($_SERVER['REQUEST_METHOD'] != "POST") {
 require_once '../../classes/Auth.class.php';
 
 //****Get Raw JSON Request Body*****//
-$data =  $_POST;
+$data = json_decode(file_get_contents("php://input"), true);
 
 //****CheckParams*****//
-
 if (!isset($data['email']) || !isset($data['password'])) {
     echo json_encode(array(
         "status" => false,
-        "message" => "Email and Password is required!"
+        "message" => "Email and Password are required!"
     ));
     http_response_code(400);
     die;
 }
-
 
 //*****GettingParams*******//
 $email = $data['email'];
 $password = $data['password'];
 
 $auth = new Auth();
-
 $auth->verifyLogin($email, $password);
