d5d170fc81f76eda42b0acf7b2c30334527d5aff
Abdul-Aziz-dal <95477937+Abdul-Aziz-dal@users.noreply.github.com>
2025-02-01 16:57:38 +0500

    Update path.config.js

--- pages/assets/js/path.config.js
+++ pages/assets/js/path.config.js
@@ -1,10 +1,2 @@
 // Detect the base URL dynamically
 var baseURL = window.location.origin;
-
-// Get the first directory after the domain (if exists)
-var pathSegments = window.location.pathname
-  .split("/")
-  .filter((segment) => segment !== "");
-if (pathSegments.length > 0) {
-  baseURL += "/" + pathSegments[0]; // Append first directory if exists
-}
